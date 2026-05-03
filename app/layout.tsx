5d5656c3271c608c87a94dd6207f3bed09df5739
jonaschlegel <jona.schlegel@gmail.com>
2026-05-03 18:10:09 +0200

    Add required marker

--- app/layout.tsx
+++ app/layout.tsx
@@ -137,6 +137,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      data-scroll-behavior="smooth"
       className={`bg-primary-cream text-primary-dark ${merriweather.variable}`}
     >
       <head>
